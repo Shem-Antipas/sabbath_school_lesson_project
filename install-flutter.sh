dcc7be22ae8a5a31b9b3cb7b9802623aaf2575ac
Shem Antipas <antipasshem@gmail.com>
2025-12-26 00:34:47 +0300

    adding a line to export the locale and using dnf in install-flutter.sh

--- install-flutter.sh
+++ install-flutter.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-
-# 1. Exit on error
 set -e
 
-# 2. Clone Flutter (Stable channel)
+# Fix locale warnings
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+# 1. Install system dependencies needed for Flutter
+# We use 'dnf' for Amazon Linux 2023
+echo "--- Installing dependencies ---"
+sudo dnf install -y mesa-libGLU libicu
+
+# 2. Clone Flutter SDK
 echo "--- Cloning Flutter SDK ---"
 if [ ! -d "flutter_sdk" ]; then
   git clone https://github.com/flutter/flutter.git -b stable flutter_sdk
 fi
 
-# 3. Add Flutter to the PATH
+# 3. Setup Path
 export PATH="$PATH:`pwd`/flutter_sdk/bin"
 
-# 4. Pre-download Web artifacts
+# 4. Build the app
 flutter config --enable-web
-flutter doctor
-
-# 5. Get dependencies
-echo "--- Fetching dependencies ---"
 flutter pub get
-
-# 6. Build for Web
-echo "--- Building Web Release ---"
 flutter build web --release --base-href "/"
 
-echo "--- Build Complete ---"
+echo "--- Build Finished Successfully ---"
