3596d5687dc81d6a628885396637d0f537d25d80
Shem Antipas <antipasshem@gmail.com>
2025-12-26 17:07:03 +0300

    Added Web renderer line on the install-flutter.sh

--- install-flutter.sh
+++ install-flutter.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git clone https://github.com/flutter/flutter.git -b stable flutter_sdk
 export PATH="$PATH:`pwd`/flutter_sdk/bin"
+flutter build web --release --web-renderer html
 flutter config --enable-web
 flutter pub get
 flutter build web --release
